2ce3b6151176e047b99a4d4877d597494f5119e5
yuenchinlammm <yuenchinlammm124@gmail.com>
2025-08-16 05:39:53 +1000

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from '../auth/auth';
 
-const api = axios.create({ baseURL: ''});
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 api.interceptors.request.use((config) => {
   const token = getToken();
