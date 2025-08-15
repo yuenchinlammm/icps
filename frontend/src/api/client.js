5299e5c9dfc27e14ca60a31f18544fa203d5c054
yuenchinlammm <yuenchinlammm124@gmail.com>
2025-08-16 04:05:54 +1000

    Create client.js

---
+++ frontend/src/api/client.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { getToken } from '../auth/auth';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
