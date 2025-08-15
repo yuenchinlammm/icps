eff2fa0722a296e7d8f51c81d0517522ee0a6e8c
yuenchinlammm <yuenchinlammm124@gmail.com>
2025-08-16 07:13:56 +1000

    Update Claim.js

--- backend/models/Claim.js
+++ backend/models/Claim.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const ClaimSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true, required: true },
   policyNumber: { type: String, required: true },
