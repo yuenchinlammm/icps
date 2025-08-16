d846d78ad707f3d13bfa789e7931cfb43b9bb318
yuenchinlammm <yuenchinlammm124@gmail.com>
2025-08-16 15:28:06 +1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use('/api',        require('./routes/document.routes'));      // /api/claims
 if (require.main === module) {
     connectDB();
     // If the file is run directly, start the server
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5001;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   }
 
